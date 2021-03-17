afe6768a4e7aa2d3c153d48c3d21275dd9c0b6fb
marichusein <marichusein@gmail.com>
2021-03-17 09:49:42 +0100

    Added ex01-ex18

---
+++ ex13/src/index.js
@@ -0,0 +1,14 @@
+function inverseWhile() {
+    var fiveNumbers = "";
+    var i = 5;
+    while(i >= 0) {
+        if(i !== 0) 
+            fiveNumbers += i + ",";
+        else 
+            fiveNumbers += i;
+        i--;
+    }
+    return fiveNumbers;
+}
+console.log(inverseWhile());
+module.exports = inverseWhile;
